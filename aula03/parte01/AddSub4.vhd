b066a984ee673f7e9cd47cf6c518985ee54d88fb
Miguel Vila <miguel.vila.dev@gmail.com>
2022-01-25 20:19:19 +0100

    aula03 parte01

--- aula03/parte01/AddSub4.vhd
+++ aula03/parte01/AddSub4.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity addsub4 is
 	port (a, b : in  std_logic_vector(3 downto 0);
@@ -8,7 +9,18 @@ entity addsub4 is
 			cout : out std_logic                  );
 end addsub4;
 
-architecture behavioral of addsub4 is
+architecture behavioral of addsub4 is 
+  signal s_a, s_b, s_s : unsigned(4 downto 0);
+begin 
+  s_a  <= '0' & unsigned(a);   -- '0's para capturar o cout do 
+  s_b  <= '0' & unsigned(b);   --  do bit mais significativo 
+  s_s  <= (s_a + s_b) when (sub = '0') else (s_a - s_b); 
+  sum  <= std_logic_vector(s_s(3 downto 0)); 
+  cout <= s_s(4); 
+end behavioral;
+
+/*
+architecture structural of addsub4 is
 	signal s_b: std_logic_vector(3 downto 0);
 begin
 	s_b <= b when (sub = '0') else not b;
@@ -19,4 +31,5 @@ begin
 				 s => sum,
 				 cout => cout);
 	-- A subtracao esta em complemento para 2
-end behavioral;
+end structural;
+*/
