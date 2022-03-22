38f9086552d7938afde0981a8f396be2c8c5ee4b
Miguel Vila <miguel.vila.dev@gmail.com>
2022-03-22 10:47:43 +0000

    aula02 parte01

--- aula02/parte01/Dec2_4En.vhd
+++ aula02/parte01/Dec2_4En.vhd
@@ -14,3 +14,42 @@ begin
 	outputs(2) <= enable and ( inputs(1)) and (not inputs(0));
 	outputs(3) <= enable and ( inputs(1)) and ( inputs(0));
 end behavequations;
+
+architecture behavassign of dec2_4en is
+begin
+	outputs <= "0000" when (enable = '0') else
+				  "0001" when (inputs = "00") else
+              "0010" when (inputs = "01") else
+              "0100" when (inputs = "10") else
+              "1000";
+end behavassign;
+
+architecture behavproc of dec2_4en is
+begin
+	process(enable, inputs)
+	begin
+		if (enable = '0') then
+			outputs <= "0000";
+		elsif (inputs = "00") then
+			outputs <= "0001";
+		elsif (inputs = "01") then
+			outputs <= "0010";
+		elsif (inputs = "10") then
+			outputs <= "0100";
+		else
+			outputs <= "1000";
+		end if;
+--end if;
+	end process;
+end behavproc;
+
+-- En I1 I0 - O3 O2 O1 O0
+-- 0  0  0  - 0  0  0  0 
+-- 0  0  1  - 0  0  0  0 
+-- 0  1  0  - 0  0  0  0 
+-- 0  1  1  - 0  0  0  0
+-----
+-- 1  0  0  - 0  0  0  1 
+-- 1  0  1  - 0  0  1  0 
+-- 1  1  0  - 0  1  0  0 
+-- 1  1  1  - 1  0  0  0 
