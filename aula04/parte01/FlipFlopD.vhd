3e4e286e0de4e61c0b4ca73ef451db743c37d701
Miguel Vila <miguel.vila.dev@gmail.com>
2022-04-10 02:49:09 +0100

    aula04 parte01

---
+++ aula04/parte01/FlipFlopD.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity flipflopd is
+	port(clk   :  in std_logic;
+		  reset :  in std_logic;
+		  set   :  in std_logic;
+        d     :  in std_logic;
+		  q     : out std_logic);
+end flipflopd;
+
+architecture behavioral of flipflopd is
+begin
+	process(clk)
+	begin
+		if (rising_edge(clk)) then
+			if (reset = '1') then
+				q <= '0';
+			elsif (set = '1') then
+				q <= d;
+			end if;
+		end if;
+	end process;
+end behavioral;
